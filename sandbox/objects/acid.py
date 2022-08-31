acd0bbdfde063e1a16cb3731c165ed49baee0412
stiderwalk <craigleppinen1@gmail.com>
2022-08-31 16:49:55 +0100

    changed acid dissolving

--- sandbox/objects/acid.py
+++ sandbox/objects/acid.py
@@ -17,34 +17,45 @@ class Acid(Particle, Liquid):
         super().__init__(x, y, mass=0.9)
         Liquid.__init__(self)
         self.update_colour()
-        self.wetness = 7
+        self.wetness = 10
         self.strength = randint(15, 17)
 
     def check_other(self, board):
         # check below
+        action = False
         if self.y < len(board) - 1 and board[self.y + 1, self.x].type == "solid":
             # kill other
             board[self.y + 1, self.x].health -= self.strength
+            action = True
 
         # check above
         if self.y > 0 and board[self.y - 1, self.x].type == "solid":
             # kill other
             board[self.y - 1, self.x].health -= self.strength
+            action = True
+
         # check left
         if self.x < len(board) - 1 and board[self.y, self.x + 1].type == "solid":
             # kill other
             board[self.y, self.x + 1].health -= self.strength
+            action = True
 
         # check right
         if self.x != 0 and board[self.y, self.x - 1].type == "solid":
             # kill other
             board[self.y, self.x - 1].health -= self.strength
+            action = True
+
+        if action and random() > 0.5 and type(board[self.y - 1, self.x]) in [Air, Acid]:
+            board[self.y - 1, self.x] = Fume(self.x, self.y - 1)
+
 
     def update(self, board):
+        self.life_len += 1
 
         self.check_other(board)
 
-        if random() > 0.85 and type(board[self.y - 1, self.x]) == Air:
+        if random() > 0.85+(self.life_len/1000) and type(board[self.y - 1, self.x]) == Air:
             board[self.y - 1, self.x] = Fume(self.x, self.y - 1)
 
         # update position
